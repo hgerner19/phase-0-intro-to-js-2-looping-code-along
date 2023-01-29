8e69cc074934a8a7ee891d155bdc9198e802eb6c
Holden Gerner <hgerner19@gmail.com>
2023-01-29 14:50:05 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+const array = ["Guadalupe", "Ollie", "Aki"];
+function writeCards(array,word){
+    const newArray =[];
+    for(let i = 0; i < array.length; i++){
+        newArray[i] = (`Thank you, ${array[i]}, for the wonderful ${word} gift!`);
+    }
+    return newArray;
+}
+
+function countDown(n){
+    let counter = n;
+    while(counter >= 0){
+        console.log(counter);
+        counter --;
+    }
+    return counter;
+
+}
